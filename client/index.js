e7098cc6e4d3cae7f215091c118a30723e51e42c
Armando Cifuentes <armando.cifuentes@unosquare.com>
2017-09-25 14:38:48 -0500

    Project 3 created from scratch

---
+++ client/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'font-awesome/css/font-awesome.min.css';
+
+import AppGame from './components/AppGame.jsx';
+
+ReactDOM.render(<AppGame />, document.getElementById('root'));
